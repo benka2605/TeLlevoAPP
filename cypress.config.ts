3080aea8dded9c8c031ef04cc995b4f533d8967b
5TT4R <to.arriagadav@duocuc.cl>
2024-11-26 00:51:36 -0300

    2.4

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl:'http://localhost:8100',
+    setupNodeEvents(on, config) {
+      
+    },
+    specPattern:'**/*.spec.ts'
+  },
+});
